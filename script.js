dc217116213732b2920c70c73668d2418d9bb7cc
StoweWilson <ifunkychunky@gmai.com>
2025-04-10 09:46:22 -0400

    home nav

---
+++ script.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById("menu")
+let ham = document.getElementById("ham")
+
+function toggleNav() {
+    menu.classList.toggle("active")
+    ham.classList.toggle("active")
+}
